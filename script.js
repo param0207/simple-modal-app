7ef87df0ab8aa2595f46d734625aab0d0ccfb39a
param0207 <chaudharyparam0207@gmail.com>
2024-11-28 12:14:55 +0530

    add overlay for modal

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const openModalBtn = document.querySelector("#open-modal-btn");
 const closeModalBtn = document.querySelector("#close-modal-btn");
 const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
 // add event listener on openModalBtn
 openModalBtn.addEventListener("click", (e) => {
   modal.classList.add("open");
+  overlay.classList.add("open");
 });
 
 closeModalBtn.addEventListener("click", (e) => {
   modal.classList.remove("open");
+  overlay.classList.remove("open");
 });
